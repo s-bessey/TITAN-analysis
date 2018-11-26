c923712aa4c2e9af07aba41a3165ea8092a897ad
sbessey <sarah_bessey@brown.edu>
2018-11-26 12:13:51 -0500

    Analysis script original (replace generic output script)

---
+++ analysisScript.R
@@ -0,0 +1,60 @@
+
+# Load necessary packages
+#if packages aren't installed,use the following code prior to running the model:
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(prettyGraphs)
+
+## Read base cases results into the global environment
+
+#import files to use
+
+## make this into more general import
+
+
+incidenceReport=read.table("incidenceReport.txt", sep="\t", header=TRUE)
+                          # Incidence Report (Overall)
+iduReport=read.table("iduReport.txt",sep="\t", 
+                     header=TRUE) # Injection Drug User Report (Overall)
+
+#function for cumulative incidence
+accumulate <- function(nonCumFile){
+  #define cumulative incidence grouped by seed
+  #creates a data frame of the cumulative incidence on each individual iteration
+  #this data frame excludes the first two columns, because time and seed should
+  #not be cumulative
+  
+  
+  #I think this needs to change to an aggregate like meanAndStd
+  cumIncidence <- as.data.frame(do.call("rbind",
+                 by(nonCumFile[3:ncol(nonCumFile)],nonCumFile$seed,cumsum)))
+  
+  #create names for each column based on original column name with _Cum appended
+  for (ii in 1:ncol(cumIncidence)){
+    names(cumIncidence)[ii]<-(paste(colnames(cumIncidence[ii]),"_Cum",sep=""))
+  }
+  
+  #create a filename for output file based on name of input file
+  file.name <- paste((deparse(substitute(nonCumFile))),"_Cum",".txt",sep="")
+  
+  #bind the first two rows (timestep and seed) to the cumulative values
+  cumulativeReport <- cbind(nonCumFile[,1:2],cumIncidence)
+  
+  #create output
+  write.table(cumulativeReport,file=file.name)
+  listName <- paste(deparse(substitute(nonCumFile)),"_Cum",sep="")
+  output <- as.data.frame(cumulativeReport)
+  assign(x = listName, value = output, env=parent.frame())
+  
+}
+
+
+
+
+accumulate(incidenceReport)
+
+#plot cumulative Total
+p <- ggplot() + geom_line(data = incidenceReport_Cum, aes(x = t, y = Total_Cum, group = seed, color=factor(seed)))
+p+geom_line(data = incidenceReport_Cum,aes(x = t, y = Total_Cum, linetype=factor(seed), color=factor(seed)))
+
