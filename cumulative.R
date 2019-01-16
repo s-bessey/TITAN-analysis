4df7bd12924f5d1308cc5e9689db4f3f5629b31b
sarah_bessey@brown.edu <sarah_bessey@brown.edu>
2019-01-16 16:35:15 -0500

    changed plotting to ggplot, used mean instead of 50% quantile, got rid of renaming columns

--- cumulative.R
+++ cumulative.R
@@ -1,46 +1,45 @@
-accumulate <-function(rawData) {
+accumulate <-function(rawData, outputname) {
   #set up empty data frame for the mean output
   #forCum <- as.data.frame(seq(0,max(rawData$t),1))
   #colnames(forCum) <- "t"
   
   #set file/object name for output
-  dataOut <- paste((deparse(substitute(rawData))),"_Cum",sep="")
+  dataOut <- paste(outputname,"_Cum",sep="")
   
   #create a dataframe of cumulative sum of incidence at each timestep by seed
-  forCum <- aggregate(.~rseed, rawData,function(x) cumsum = cumsum(x)) %>%
+  forCum <- aggregate(.~rseed + t, rawData,function(x) cumsum = cumsum(x)) %>%
                separate_rows()
-  forCum$t <- rawData$t
+  #shouldn't need this when changing to aggregate by both seed and t
+  #forCum$t <- rawData$t
   forCum$pseed <- rawData$pseed
   forCum$nseed <- rawData$nseed
-  
-  colnames(forCum)[5:ncol(forCum)] <- paste(colnames(forCum)[5:ncol(forCum)],
-                                            "_Cum",sep="")
-  #colnames(forCum)[1:2] <- c("seed", "t")   #above line adds mean to end of 
-  #column. This corrects seed and time back to t
-  
-  
-  #meanReport <- merge(forCum,forSd)  #this puts the mean and std in one file 
-  #and merges the t columns
+
   
   assign(x=dataOut, value = forCum, env = parent.frame()) #create variable
   fileName <- paste((deparse(substitute(rawData))),"_Cum",".txt",sep="")
   #write.table(cumReport, file = fileName)
-  CIs <- tapply(forCum$Total_Cum, forCum$t, quantile, probs = c(.025, .5, .975)) %>%
+  CIs <- tapply(forCum$Total, forCum$t, quantile, probs = c(.025, .975)) %>%
     do.call("rbind",.) %>% as.data.frame()
-  assign("Confidence", CIs, envir = .GlobalEnv)
+  meanCum <- aggregate(~.t, forCum, FUN = mean)
+  assign("output", cbind(CIs, meanCum$Total), envir = .GlobalEnv)
 }
-meanAndStd()
 
+accumulate(basicReport_BLACK)
+# have to see where the confidences land in this
+blackPlot <- ggplot(basicReport_BLACK) + geom_line(aes(t, total)) + 
+  geom_ribbon(aes(x = t, ymin = Confidence[1], ymax = Confidence[2]), alpha = .3)
 
-xvalue <- as.numeric(unlist(basicReport_HF_Cum$t)[1:nrow(Confidence)])
-yvalue <- as.numeric(unlist(Confidence[2]))
-upperCI <- as.numeric(unlist(Confidence[3]))
-lowerCI <- as.numeric(unlist(Confidence[1]))
-
-plot(xvalue,yvalue, type = 'l')
-polygon(c(rev(xvalue),xvalue), c(rev(lowerCI), upperCI), col = 'grey80', border = NA)
-lines(lowerCI~xvalue)
-lines(upperCI~xvalue)
-
-lines(yvalue~xvalue)
-
+#completely untested
+# check if the parse works
+# check if the name works
+for (i in 1:length(dataframe_names)){
+  dataTemp <- parse(dataframe_names[i])
+  accumulate(dataTemp, dataframe_names[i])
+  #really unsure about this. I don't think i need to deparse? since that's a character df
+  write.table(output, file = paste(dataframe_names[i], "_cum.txt", sep = ""))
+  #may need to add output$ to variables
+  plotTemp <- ggplot(output) + geom_line(aes(t, total)) + 
+    geom_ribbon(aes(x = t, ymin = Confidence[1], ymax = Confidence[2]), alpha = .3) +
+    theme_bw()
+  ggsave(paste(dataframe_names[i], "fig.pdf",sep = ""))
+}
